701236b290bc75f27e1d83d6b12600d773f1b817
Максим Ордян <156433440+Olezenka@users.noreply.github.com>
2024-12-24 09:48:28 +0300

    defsef

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Librarry_Dikaya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Team team = new Team("Лисы", "Москва");
+
+                team.AddPlayers(new Player("Кевин Дэй", "Нападающий"));
+                team.AddPlayers(new Player("Ники Хэмик", "Защитник"));
+                team.AddPlayers(new Player("Эндрю Джостен", "Голкипер"));
+                team.AddPlayers(new Player("Нил Джостен", "Нападающий"));
+                team.AddPlayers(new Player("Сет Горден", "Защитник"));
+
+                team.ShowAll();
+                Console.WriteLine("\nРаботу выполнила Дикая");
+                Console.ReadKey();
+            }
+        }
+}
---
+++ Librarry_Dikaya/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librarry_Dikaya
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Player(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Имя: {Name}, Описание: {Description}");
+        }
+    }
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Player> players;
+
+        public Team(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            players = new List<Player>();
+        }
+
+        public void AddPlayers(Player player)
+        {
+            players.Add(player);
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Команда: {Name}, Адрес: {Address}");
+        }
+
+
+        public void ShowAll()
+        {
+            Show();
+            Console.WriteLine("Игроки в команде: ");
+            foreach (var player in players.OrderBy(p => p.Name))
+            {
+                player.Show();
+
+            }
+
+        }
+    }
+}
